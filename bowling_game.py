585f9bc8d13eeddeeed4b39f672d7cc78bfa79ce
Devin McFarlane <devin.ryan.mcfarlane@gmail.com>
2020-02-11 11:53:44 -0500

    removed initialization
    franco said this helps cpu time, i dont think it was a problem in the first place tho...

--- bowling_game.py
+++ bowling_game.py
@@ -1,11 +1,9 @@
 class BowlingGame:
-    def __init__(self):
-        # pkd = pins knocked down
-        self.pkd = 0
+     # pkd = pins knocked down
+    pkd = 0
 
     def roll(self, pins):
         self.pkd += pins
-        pass
 
     def score(self):
         return self.pkd
