1e28c29cf74c0d0244249a5d22160c8b2aff5c6a
Devin McFarlane <devin.ryan.mcfarlane@gmail.com>
2020-02-11 12:26:54 -0500

    add spares

--- bowling_game.py
+++ bowling_game.py
@@ -1,9 +1,17 @@
 class BowlingGame:
-    # pkd = pins knocked down
-    pkd = 0
+    total_score = 0
+    frames = []
+    current_frame = []
 
     def roll(self, pins):
-        self.pkd += pins
+        self.current_frame.append(pins)
+        if len(self.current_frame) == 2:
+            self.frames.append(self.current_frame)
+            self.current_frame = []
 
     def score(self):
-        return self.pkd
+        for i, frame in enumerate(self.frames):
+            self.total_score += sum(self.frames[i])
+            if sum(frame) == 10:
+                self.total_score += self.frames[i + 1][0]
+        return self.total_score
