ee761aeb76b4821c2ed8b549054dcdf177779a14
Devin McFarlane <devin.ryan.mcfarlane@gmail.com>
2020-02-11 11:29:48 -0500

    Added TestCase

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -1,2 +1,10 @@
-import unittest
+from unittest import TestCase
+from bowling_game import BowlingGame
 
+
+class TestBlowingGame(TestCase):
+    def setUp(self):
+        self.game = BowlingGame()
+
+    def test_gutter_game(self):
+        pass
